51481e484cc71f9eb796e621203267eb94a8bc07
nolim1t <hello@nolim1t.co>
2019-01-25 18:49:59 +0700

    useCNXNode for charges

--- js/addon-btc-lightning.js
+++ js/addon-btc-lightning.js
@@ -39,7 +39,7 @@ const check_btc_rates = (callback) => {
 };
 
 const check_charge_id = (chargeId, callback) => {
-  axios.get(base_url + '?checkCharge=true&chargeId=' + chargeId).then((response) => {
+  axios.get(base_url + '?checkCharge=true&useLNCNXNode=true&chargeId=' + chargeId).then((response) => {
     if (response.data['response'] !== undefined) {
       if (response.data['response']['paid'] !== undefined) {
         var cbresp_charge = {
