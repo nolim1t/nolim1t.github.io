0b748678974b01f5f01a1a3ca67b4103dd1fb123
nolim1t <hello@nolim1t.co>
2019-01-27 18:01:47 +0700

    use parameterized port and host

--- js/addon-btc-lightning.js
+++ js/addon-btc-lightning.js
@@ -216,7 +216,7 @@ var lnapp = new Vue({
           whichLNDNode = whichLNDNode - 1;
         }
         // LNNodePorts[whichLNDNode]
-        console.log("Service availability status: " + checkServiceAvailability("reckless.nolim1t.co", LNNodePorts[whichLNDNode]));
+        console.log("Service availability status: " + checkServiceAvailability(LNCNXNodeHost, LNNodePorts[whichLNDNode]));
         var url = base_url + "?showInvoice=true&useLNCNXNode=true&LNCNXNodeHost=reckless.nolim1t.co&LNCNXNodePort=" + parseInt(LNNodePorts[whichLNDNode]) + "&invoiceAmount=" + this.amount.toString() + "&invoiceDescription=" + encodeURIComponent(invoiceDescriptionToGenerate);
         this.resultElement.innerHTML = 'Fetching....';
 
