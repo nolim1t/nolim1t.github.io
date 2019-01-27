e6a8fcbeb7376cc8deac1730a68902bbe9a47b80
nolim1t <hello@nolim1t.co>
2019-01-27 18:45:30 +0700

    Change to full node

--- js/addon-btc-lightning.js
+++ js/addon-btc-lightning.js
@@ -200,7 +200,7 @@ var lnapp = new Vue({
           // by default
           invoiceDescriptionToGenerate = document.getElementById("descriptionform").value
         }
-        var LNNodePorts = [1666, 1667]; // Choose a random node
+        var LNNodePorts = 1666; // Choose a random node
         var LNCNXNodeHost = 'reckless.nolim1t.co'; // same same
         var BorkedNodeList = ''; // Borked Node list (defines which nodes are not working, for fallover)
         var whichLNDNode = Math.floor((Math.random() * LNNodePorts.length) + 0);
