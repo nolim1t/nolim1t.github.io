a00bec6f8abd92dc004a9929eb0f24a97785045e
nolim1t <hello@nolim1t.co>
2020-11-09 22:07:27 +0700

    Update generation script

--- generate.sh
+++ generate.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
 ./canary.sh > ./canary.txt
 gpg --clearsign ./canary.txt
@@ -9,9 +16,12 @@ echo "---" >> ./header.txt
 echo "" >> ./header.txt
 
 # Set up canary.txt
+# Overwrite header
 cat header.txt > ./canary.txt
+# Move signed to canary.txt
 cat ./canary.txt.asc >> ./canary.txt
 
+# Cleanup
 rm ./canary.txt.asc
 rm ./header.txt
 
