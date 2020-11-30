8eafd958acf64167904160e094d4366e32c0e99d
nolim1t <hello@nolim1t.co>
2020-11-30 16:03:57 +0700

    Remove generate.sh , fix up canary.sh , update warrant canary

--- canary.sh
+++ canary.sh
@@ -7,20 +7,41 @@
 # SOFTWARE.
 
 # This is the gohttpd server
+# https://gitlab.com/nolim1t/golang-httpd-test
+
 APISERVER="http://localhost:6969/api/"
 BLOCKHASH=`curl "${APISERVER}getblockhash" 2>/dev/null | jq .blockhash | sed 's/\"//g;'`
+BLOCKCOUNT=`curl "${APISERVER}blocks" 2>/dev/null | jq .count`
 DATE=`date +"%Y-%m-%d"`
 ADMIN="@nolim1t"
+# Defaine your site
 SITE="nolim1t.co and nlboxajljr5boevw3vyfryenhwqs5eehfh5pnbzjlnounrje5wulbnyd.onion"
 
-cat <<EOF
+cat >./canary.txt  <<EOF
 I am the admin of ${SITE}. (${ADMIN}).
 I am in control of my PGP key.
 I will update this canary within 14 days.
 This file can be accessed at /canary.txt
 
 Today is ${DATE}
-Best bitcoin block hash:
-${BLOCKHASH}
 
+The current best bitcoin block hash:
+${BLOCKHASH} (Block: ${BLOCKCOUNT})
 EOF
+
+gpg --clearsign ./canary.txt
+echo "---" >> ./header.txt
+echo "layout: null" >> ./header.txt
+echo "permalink: /canary.txt" >> ./header.txt
+echo "---" >> ./header.txt
+echo "" >> ./header.txt
+
+# Set up canary.txt
+# Overwrite header
+cat header.txt > ./canary.txt
+# Move signed to canary.txt
+cat ./canary.txt.asc >> ./canary.txt
+
+# Cleanup
+rm ./canary.txt.asc
+rm ./header.txt
