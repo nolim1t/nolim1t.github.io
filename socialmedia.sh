bb07b376c4d4c51ee4d28499addc051b4162cf93
nolim1t <hello@nolim1t.co>
2021-01-11 16:21:50 +0700

    Add socialmedia.sh

---
+++ socialmedia.sh
@@ -0,0 +1,44 @@
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+# This is the gohttpd server
+# https://gitlab.com/nolim1t/golang-httpd-test
+
+APISERVER="http://localhost:6969/api/"
+BLOCKHASH=`curl "${APISERVER}getblockhash" 2>/dev/null | jq .blockhash | sed 's/\"//g;'`
+BLOCKCOUNT=`curl "${APISERVER}blocks" 2>/dev/null | jq .count`
+DATE=`date +"%Y-%m-%d"`
+ADMIN="@nolim1t"
+
+cat >./socialmedia.txt  <<EOF
+I am the owner of the following profiles:
+
+https://bitcoinhackers.org/@nolim1t
+https://twitter.com/nolim1t
+https://t.me/nolim1tcoblog
+https://nolim1t.co
+
+Today is ${DATE}
+
+The current best bitcoin block hash:
+${BLOCKHASH} (Block: ${BLOCKCOUNT})
+EOF
+
+gpg --clearsign ./socialmedia.txt
+echo "---" >> ./header.txt
+echo "layout: null" >> ./header.txt
+echo "permalink: /socialmedia.txt" >> ./header.txt
+echo "---" >> ./header.txt
+echo "" >> ./header.txt
+
+cat header.txt > ./socialmedia.txt
+cat ./socialmedia.txt.asc >> ./socialmedia.txt
+
+# Cleanup
+rm ./socialmedia.txt.asc
+rm ./header.txt
