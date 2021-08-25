52db072b282df41cfd880e12bb7537ded0d15a92
nolim1t <hello@nolim1t.co>
2021-08-25 11:04:12 +0700

    Update matrix keys

--- matrixkeys.sh
+++ matrixkeys.sh
@@ -18,22 +18,9 @@ ADMIN="@nolim1t"
 cat >./matrixkeys.txt  <<EOF
 My device keys are as follows:
 
-Mobile Client (@nolim1t:matrix.org)
-Session ID: GPOXMNETXX
-Session key:
-7qWl SvVB XrXF 1Xz6 9nAC g7Rx NyPm 2Ln9 mBKr qtoB WUQ
-
-Mac Client (@nolim1t:matrix.org)
+Desktop Client (@nolim1t:matrix.org)
 Session ID: HVITNQHIAC
 Session key:    9W5O EEzt V9a1 pVzE 0Dk+ 5LSJ DUN9 bW3S R9qS 7rDb kxc
-
-Firefox Linux (@nolim1t:matrix.org)
-Session ID: CIOUFWHAHZ
-Session Key: ztja gzSi ynLj 5Fdt kW3L fFBV Ny9L gzpt 25jt IBWu zfg
-
-Android Client (@nolim1t:matrix.org)
-Session Id: UUCZTIOVWQ
-Session key: AjaW kKtB UGG5 FIiZ jry8 OR8t xaVo XqfY elOM t2A3 SMk
 
 Web Client (@nolim1t@converser.eu)
 Session ID:TSHTWVGGKV
