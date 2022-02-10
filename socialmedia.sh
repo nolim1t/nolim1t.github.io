31071febdbc45328f43ba9e104ca5b3ea1fa1a86
nolim1t <hello@nolim1t.co>
2022-02-10 15:05:03 +0700

    Update texts

--- socialmedia.sh
+++ socialmedia.sh
@@ -29,6 +29,13 @@ Matrix
 @nolim1t:converser.eu (Because decentralization - currently down)
 #thebarrenschat:matrix.nolim1t.co (General real time chatroom)
 
+Nostr Public Key
+146bda4ec6932830503ee4f8e8b626bd7b3a5784232b8240ba15c8cbff9a07cd
+
+Nostr Urls
+https://nostr.rocks/146bda4ec6932830503ee4f8e8b626bd7b3a5784232b8240ba15c8cbff9a07cd
+https://nostr.com/146bda4ec6932830503ee4f8e8b626bd7b3a5784232b8240ba15c8cbff9a07cd
+
 Mastodon
 https://bitcoinhackers.org/@nolim1t (Main)
 https://bitcoinersagainst.boats/@nolim1t (Backup)
